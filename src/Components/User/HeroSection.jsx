cbc746ff068ba37b3d671ba180aa8be65bba1668
mlkmoaz01 <mlkmoaz01@gmail.com>
2025-02-02 21:31:52 +0500

    Just For u

--- src/Components/User/HeroSection.jsx
+++ src/Components/User/HeroSection.jsx
@@ -8,12 +8,12 @@ const HeroSection = () => {
           <div className="flex flex-col justify-between h-full">
             <div>
               <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-                {/* Elevate Your Style with Premium Clothes & Accessories */}
-                Noor I love U 
+                Elevate Your Style with Premium Clothes & Accessories
+                {/* Noor I love U  */}
               </h1>
               <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-                {/* Discover the perfect blend of fashion and functionality. Shop our exclusive collection of clothing, watches, bags, and more to complete your look. */}
-              Ab tu Yaqeen krlu na Plsss 
+                Discover the perfect blend of fashion and functionality. Shop our exclusive collection of clothing, watches, bags, and more to complete your look.
+              {/* Ab tu Yaqeen krlu na Plsss  */}
               </p>
             </div>
             <div className="flex space-x-4 mt-6">
