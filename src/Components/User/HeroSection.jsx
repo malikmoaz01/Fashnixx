8897e78fe9c1979545accca78f01ecad044ffdbd
mlkmoaz01 <mlkmoaz01@gmail.com>
2025-02-02 23:04:39 +0500

    Just For u

--- src/Components/User/HeroSection.jsx
+++ src/Components/User/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
             <div>
               <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                 {/* Elevate Your Style with Premium Clothes & Accessories */}
-                Noor I love U 😘😘😘😘😘
+                Welcome Noor Meri jan I love U 😘😘😘😘😘
               </h1>
               <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                 {/* Discover the perfect blend of fashion and functionality. Shop our exclusive collection of clothing, watches, bags, and more to complete your look. */}
